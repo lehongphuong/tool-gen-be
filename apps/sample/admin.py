9c39bf50683b166e6bff959e63580504b0b2c5c4
PinkWays <helloitpdu@gmail.com>
2020-10-02 15:35:19 +0700

    phuonglh update backend admin

--- apps/sample/admin.py
+++ apps/sample/admin.py
@@ -8,4 +8,4 @@ class CustomerAdmin():
     list_filter = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
     search_fields = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
 
-admin.site.register(models.Customer, CustomerAdmin)
+# admin.site.register(models.Customer, CustomerAdmin)
