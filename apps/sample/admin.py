01998e07f162b8266b07e70ec25bf604321f7b5d
PinkWays <helloitpdu@gmail.com>
2020-10-02 15:33:45 +0700

    phuonglh update backend admin

--- apps/sample/admin.py
+++ apps/sample/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
+# from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 from . import models
 
-class CustomerAdmin(ImportExportModelAdmin):
+class CustomerAdmin():
     list_display = ('id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status')
     list_filter = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
     search_fields = ['id','cmnd','username','phone','number_ticket','number_ticket_go','number_ticket_return','deposit','money','start_date','start_time_train','end_date','end_time_train','note','status']
