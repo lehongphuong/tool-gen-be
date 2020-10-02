62308354db454cce0c77af1abc0b9ba9c9bf6eda
PinkWays <helloitpdu@gmail.com>
2020-10-02 15:48:04 +0700

    phuonglh update backend admin

--- apps/sample/admin.py
+++ apps/sample/admin.py
@@ -1,4 +1,4 @@
-# from django.contrib import admin
+from django.contrib import admin
 # # from import_export.admin import ImportExportModelAdmin
 # # Register your models here.
 # from . import models
